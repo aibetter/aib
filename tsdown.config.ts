8ffbe6a840346e0e10f57034422678c2243a8731
litingyes <luz.liting@gmail.com>
2025-08-15 23:16:49 +0800

    chore: update tsdown configuration to disable dts and include external dependencies
    - Set dts option to false in tsdown.config.ts.
    - Added noExternal option to include all dependencies from package.json.

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'tsdown'
+import pkg from './package.json'
 
 export default defineConfig({
   entry: ['./src'],
+  dts: false,
+  noExternal: [...Object.keys(pkg.dependencies)],
 })
